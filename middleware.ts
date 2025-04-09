c0fa9ad53df1aec192ccda98d16781f90d80bb8d
Madhu Pandey <pandeymadhu80@gmail.com>
2025-04-09 18:24:49 +0530

    changed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
-  "/api/webhooks/clerk",
+  "/api/webhooks(.*)", // allows all webhook routes to be public
 ]);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   if (isPublicRoute(req)) return;
+  // You can add auth logic here later if needed
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files
+    // Skip Next.js internals and static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
